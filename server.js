ac67b288f10b440bba64111658c35b2b345cde3b
Daniel Pope <danielthepope@users.noreply.github.com>
2016-10-15 15:05:14 +0100

    Add in-memory caching

--- server.js
+++ server.js
@@ -2,32 +2,53 @@ const app = require('express')();
 const http = require('http');
 const cheerio = require('cheerio');
 const exphbs = require('express-handlebars');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({stdTTL: 60, checkperiod: 100});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
+  const cached = cache.get('random');
+  if (cached) {
+    console.log('returning cached ' + cached.imageUrl);
+    return res.render('index', cached);
+  }
   randomImage(function (url) {
     const number = url.match(/\d+/)[0];
     xkcdImage(number, function(err, data) {
       if (err) return res.send("some error happened.");
+      cache.set('random', data, 10);
+      cache.set(number, data, 3600);
       res.render('index', data);
     });
   });
 });
 
 app.get('/latest', function (req, res) {
+  const cached = cache.get('latest');
+  if (cached) {
+    console.log('returning cached ' + cached.imageUrl);
+    return res.render('index', cached);
+  }
   xkcdImage('', function (err, data) {
     if (err) return res.send("some error happened.");
+    cache.set('latest', data, 600);
     res.render('index', data);
   });
 });
 
 app.get('/rotate', function (req, res) {
+  const cached = cache.get('random');
+  if (cached) {
+    console.log('returning cached ' + cached.imageUrl);
+    return res.render('index', cached);
+  }
   randomImage(function (url) {
     const number = url.match(/\d+/)[0];
     xkcdImage(number, function(err, data) {
       if (err) return res.send("some error happened.");
+      cache.set('random', data, 10);
       data.rotate = true;
       res.render('index', data);
     });
@@ -35,8 +56,15 @@ app.get('/rotate', function (req, res) {
 });
 
 app.get('/:comic(\\d+)', function (req, res) {
-  xkcdImage(req.params.comic, function (err, data) {
+  const number = req.params.comic;
+  const cached = cache.get(number);
+  if (cached) {
+    console.log('returning cached ' + cached.imageUrl);
+    return res.render('index', cached);
+  }
+  xkcdImage(number, function (err, data) {
     if (err) return res.send("some error happened.");
+    cache.set(number, data, 3600);
     res.render('index', data);
   });
 });
