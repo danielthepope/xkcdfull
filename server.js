81a1d92f9695b9174367b3c9969061532d6f7193
Unknown <danielthepope@users.noreply.github.com>
2017-07-10 23:07:08 +0100

    Use template strings and const/let

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var app = express();
-var extend = require('extend');
-var exphbs = require('express-handlebars');
-var NodeCache = require('node-cache');
-var cache = new NodeCache({stdTTL: 60, checkperiod: 100});
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const extend = require('extend');
+const exphbs = require('express-handlebars');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({stdTTL: 60, checkperiod: 100});
+const port = process.env.PORT || 3000;
 
-var log = require('./log.js');
-var xkcdApi = require('./xkcdapi.js');
+const log = require('./log.js');
+const xkcdApi = require('./xkcdapi.js');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  var page = 'index';
-  var rotate = false;
+  let page = 'index';
+  let rotate = false;
   if(req.query.full !== undefined) page = 'imageonly';
   if(req.query.rotate !== undefined) rotate = true;
   if (!renderFromCache('random', res, page, {rotate: rotate})) {
@@ -27,27 +27,27 @@ app.get('/test', function (req, res) {
 });
 
 app.get('/latest', function (req, res) {
-  var page = 'index';
+  let page = 'index';
   if(req.query.full !== undefined) page = 'imageonly';
   renderFromCache('latest', res, page) || renderXkcdImageAndCache(res, '', page, 'latest');
 });
 
 app.get('/:comic(\\d+)', function (req, res) {
-  var page = 'index';
+  let page = 'index';
   if(req.query.full !== undefined) page = 'imageonly';
-  var number = req.params.comic;
+  const number = req.params.comic;
   renderFromCache(number, res, page) || renderXkcdImageAndCache(res, number, page);
 });
 
 app.listen(port, function () {
-  log('xkcd-full running on port ' + port);
+  log(`xkcd-full running on port ${port}`);
 });
 app.use(express.static('public'));
 
 function renderFromCache(key, res, page, options) {
-  var cached = cache.get(key);
+  const cached = cache.get(key);
   if (cached) {
-    log('returning cached ' + cached.img);
+    log(`returning cached ${cached.img}`);
     res.render(page, extend(options, cached));
   }
   return !!cached;
