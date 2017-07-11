63e25e577b9ddb278173c6850e6c6fe310bdd788
Unknown <danielthepope@users.noreply.github.com>
2017-07-12 00:57:19 +0100

    Make comicData accessible within module

--- xkcdapi.js
+++ xkcdapi.js
@@ -10,26 +10,29 @@ const NAUGHTY_WORDS = ['fuck', 'shit', 'cunt'];
 const INTERACTIVE = [1110,1190,1193,1446,1525,1608,1663];
 const DO_NOT_EXIST = [404];
 
-let ignoreList = []; // updated after setup() is called
-let latest = 0; // updated after setup() is called
+// These are updated after setup() is called
+let ignoreList = [];
+let latest = 0;
+let comicData = {};
 
 function setup(callback) {
   if (!callback) callback = function(err) {};
   updateInfo(function() {
-    yaml.read(INFO_FILE, function(err, comicData) {
+    yaml.read(INFO_FILE, function(err, data) {
       if (err) return callback(err);
-      const latestComicNumber = Object.keys(comicData).map(num_s => parseInt(num_s)).sort((a,b) => a > b).reverse()[0];
+      const latestComicNumber = Object.keys(data).map(num_s => parseInt(num_s)).sort((a,b) => a > b).reverse()[0];
       latest = latestComicNumber;
-      const nsfwComics = Object.keys(comicData).filter(key => {
-        const comic = comicData[key];
+      comicData = data;
+      const nsfwComics = Object.keys(data).filter(key => {
+        const comic = data[key];
         let bad = false;
         NAUGHTY_WORDS.forEach(word => {
           if (comic.transcript.toLowerCase().indexOf(word) != -1) bad = true;
           if (comic.alt.toLowerCase().indexOf(word) != -1) bad = true;
         });
-        if (bad) console.log(`comic #${key} has a naughty word`);
         return bad;
       });
+      log(`${nsfwComics.length} comics have naughty words`);
       ignoreList = INTERACTIVE.concat(DO_NOT_EXIST).concat(nsfwComics);
       log('setup() complete');
       callback();
@@ -42,9 +45,9 @@ function updateInfo(callback) {
   // Get latest comic number
   getLatestComicNumber(function(latestComicNumber) {
     // Get latest comic from info yaml file
-    yaml.read(INFO_FILE, function(yamlErr, comicData) {
+    yaml.read(INFO_FILE, function(yamlErr, data) {
       if (yamlErr) return callback(yamlErr);
-      const latestDownloaded = Object.keys(comicData).map(num_s => parseInt(num_s)).sort((a,b) => a > b).reverse()[0];
+      const latestDownloaded = Object.keys(data).map(num_s => parseInt(num_s)).sort((a,b) => a > b).reverse()[0];
       log(`latest is #${latestComicNumber}, latest downloaded is #${latestDownloaded}`);
       if (latestComicNumber > latestDownloaded) {
         // Fetch metadata and transcript for each outstanding comic in turn
